bd1bb3d4227ccf2232c06d545ae98fee825e01b7
Aileen Pierce <aileen.pierce@colorado.edu>
2021-02-22 18:08:07 -0700

    data

---
+++ week 07/Grocery/Grocery/GroceryDataHandler.swift
@@ -0,0 +1,75 @@
+//
+//  GroceryDataHandler.swift
+//  Grocery
+//
+//  Created by Aileen Pierce
+//
+
+import Foundation
+
+class GroceryDataHandler{
+    var groceryData = [String]()
+    
+    func dataFileURL(_ filename:String) -> URL? {
+        //returns an array of URLs for the document directory in the user's home directory
+        let urls = FileManager.default.urls(for:.documentDirectory, in: .userDomainMask)
+        var url : URL?
+        //append the file name to the first item in the array which is the document directory
+        url = urls.first?.appendingPathComponent(filename)
+        //return the URL of the data file or nil if it does not exist
+        return url
+    }
+
+    func loadData(fileName: String){
+        //url of data file
+        let fileURL = dataFileURL(fileName)
+        //if the data file exists, use it
+        if FileManager.default.fileExists(atPath: (fileURL?.path)!){
+            let url = fileURL!
+            do {
+                //creates a data buffer with the contents of the plist
+                let data = try Data(contentsOf: url)
+                //create an instance of PropertyListDecoder
+                let decoder = PropertyListDecoder()
+                //decode the data using the structure of the Favorite class
+                groceryData = try decoder.decode([String].self, from: data)
+            } catch {
+                print("no file")
+                }
+        }
+        else {
+            print("file does not exist")
+        }
+    }
+    
+    func saveData(fileName: String){
+        //url of data file
+        let fileURL = dataFileURL(fileName)
+        //if the data file exists, use it
+        if FileManager.default.fileExists(atPath: (fileURL?.path)!){
+            do {
+                //create an instance of PropertyListEncoder
+                let encoder = PropertyListEncoder()
+                //set format type to xml
+                encoder.outputFormat = .xml
+                let encodedData = try encoder.encode(groceryData)
+                //write encoded data to the file
+                try encodedData.write(to: fileURL!)
+            } catch {
+                print("write error")
+            }
+        }
+    }
+    
+    func getGroceryItems() -> [String]{
+        return groceryData
+    }
+    
+    func addItem(newItem: String){
+        groceryData.append(newItem)
+    }
+    
+    func deleteItem(index: Int){
+        groceryData.remove(at: index)
+    }
+}
---
+++ week 07/Grocery/Grocery/GroceryTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  GroceryTableViewController.swift
+//  Grocery
+//
+//  Created by Aileen Pierce
+//
+
+import UIKit
+
+class GroceryTableViewController: UITableViewController {
+    
+    var groceries = [String]()
+    var groceryData = GroceryDataHandler()
+    let dataFile = "test.plist"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+         self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        groceryData.loadData(fileName: dataFile)
+        groceries = groceryData.getGroceryItems()
+        
+        //subscribe to the UIApplicationWillResignActiveNotification notification
+        NotificationCenter.default.addObserver(self, selector: #selector(self.applicationWillResignActive(_:)), name: UIApplication.willResignActiveNotification, object: nil)
+    }
+
+    //called when the UIApplicationWillResignActiveNotification notification is posted
+    //all notification methods take a single NSNotification instance as their argument
+    @objc func applicationWillResignActive(_ notification: Notification){
+        groceryData.saveData(fileName: dataFile)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return groceries.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel!.text = groceries[indexPath.row]
+        return cell
+    }
+
+    @IBAction func addGroceryItem(_ sender: UIBarButtonItem) {
+        let addalert = UIAlertController(title: "New Item", message: "Add a new item to your grocery list", preferredStyle: .alert)
+        //add textfield to the alert
+        addalert.addTextField(configurationHandler: {(UITextField) in
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        addalert.addAction(cancelAction)
+        let addItemAction = UIAlertAction(title: "Add", style: .default, handler: {(UIAlertAction)in
+            // adds new item
+            let newitem = addalert.textFields![0] //gets textfield
+            let newGroceryItem = newitem.text! //gets textfield text
+            self.groceries.append(newGroceryItem) //adds textfield text to array
+            self.tableView.reloadData()
+            self.groceryData.addItem(newItem: newGroceryItem) // add to data handler
+        })
+        addalert.addAction(addItemAction)
+        present(addalert, animated: true, completion: nil)
+    }
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            groceries.remove(at: indexPath.row) //delete the item from the array
+            groceryData.deleteItem(index: indexPath.row) // delete the row from the data handler 
+            tableView.deleteRows(at: [indexPath], with: .fade) //update table view
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
